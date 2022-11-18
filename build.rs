e987de5e04b0c14a36431dfe1453bee5a3917026
Mateus Azis <azis@google.com>
2022-11-17 22:45:17 -0800

    Make shared library compilable on macos

--- build.rs
+++ build.rs
@@ -4,11 +4,24 @@ pub fn main() {
     let mut cmd: std::process::Command = std::process::Command::new("make");
     cmd.args(["-B", "libmylib.so"]).current_dir("./src/util");
 
+    let mut c_flags = String::new();
+
     if let Ok(target) = std::env::var("TARGET") {
         if target.starts_with("aarch64-unknown-linux-") {
-            cmd.env("CFLAGS", "--target=arm64v7a-linux-gnueabi -fuse-ld=lld -g");
+          c_flags.push_str("--target=arm64v7a-linux-gnueabi -fuse-ld=lld -g");
         }
     }
+
+    if let Ok(target_os) = std::env::var("CARGO_CFG_TARGET_OS") {
+      if target_os == "macos" {
+        c_flags.push_str(" -lSystem");
+      }
+    }
+
+    if !c_flags.is_empty() {
+      cmd.env("CFLAGS", c_flags.as_str());
+    }
+
     let output = cmd.output().expect("should have ran Make");
 
     let code = output.status.code().unwrap();
