e0f82f80ba5c6ec7d2b5b8d61108c4ebf2c7198a
Mateus Azis <mateusazis@gmail.com>
2023-01-14 09:47:04 -0800

    manual async simplifications

--- src/util/manual_async.rs
+++ src/util/manual_async.rs
@@ -1,9 +1,7 @@
-use async_std::future;
 use futures::future::BoxFuture;
 use futures::task::{waker_ref, ArcWake};
 use futures::{Future, FutureExt};
 use std::pin::Pin;
-use std::process::Output;
 use std::sync::mpsc::{sync_channel, SyncSender};
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -11,10 +9,8 @@ use std::task::{Context, Poll};
 use std::thread::{sleep, spawn, JoinHandle};
 use std::time::Duration;
 
-struct IntWrapper(i32);
-
 struct DelayedResult {
-    current_call_count: Mutex<IntWrapper>,
+    current_call_count: Mutex<i32>,
     required_call_count: i32,
     result: i32,
 }
@@ -22,7 +18,7 @@ struct DelayedResult {
 impl DelayedResult {
     fn new(required_call_count: i32, result: i32) -> DelayedResult {
         DelayedResult {
-            current_call_count: Mutex::new(IntWrapper(0)),
+            current_call_count: Mutex::new(0),
             required_call_count,
             result,
         }
@@ -35,10 +31,11 @@ impl Future for DelayedResult {
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let lock = (*self).current_call_count.lock();
         let mut wrapper = lock.unwrap();
-        let new_value = wrapper.0 + 1;
-        wrapper.0 = new_value;
+        let new_value = *wrapper + 1;
+        *wrapper = new_value;
         std::mem::drop(wrapper);
 
+        println!("Count is: {}", new_value);
         if new_value >= self.required_call_count {
             return Poll::Ready(self.result);
         }
@@ -50,24 +47,6 @@ impl Future for DelayedResult {
 struct Executor {
     sender: SyncSender<i32>,
     thread: JoinHandle<()>,
-}
-
-impl Executor {
-    fn new(delay: Duration) -> Executor {
-        let (sender, receiver) = sync_channel(8096);
-        let thread = spawn(move || {
-            while let Ok(val) = receiver.recv() {
-                println!("Received: {}", val);
-                sleep(delay);
-            }
-        });
-        Executor { sender, thread }
-    }
-
-    fn join(self) {
-        std::mem::drop(self.sender);
-        self.thread.join().expect("should join");
-    }
 }
 
 struct Task {
@@ -117,27 +96,24 @@ impl Executor2 {
         std::mem::drop(self.sender);
         self.thread.join().expect("should join");
     }
+
+    fn send(&self, future : BoxFuture<'static, i32>) {
+      let task = Arc::new(Task {
+          future: Mutex::new(future.boxed()),
+          sender: self.sender.clone(),
+      });
+      self.sender.send(task).expect("should send");
+    }
+}
+
+async fn build_simple_future() -> i32 {
+  return 49;
 }
 
 pub fn main() {
-    // let executor = Executor::new(Duration::from_millis(500));
-    // executor.sender.send(10).unwrap();
-    // executor.sender.send(20).unwrap();
-    // executor.sender.send(30).unwrap();
-    // executor.join();
-
-    let executor2 = Executor2::new(Duration::from_millis(500));
+    let executor= Executor2::new(Duration::from_millis(500));
     let fut = DelayedResult::new(5, 42);
-    let task = Arc::new(Task {
-        future: Mutex::new(fut.boxed()),
-        sender: executor2.sender.clone(),
-    });
-    executor2.sender.send(task).expect("should send");
-    executor2.join();
-
-    // let mut val_future : Box<dyn Future<Output = i32>> = Box::new(DelayedResult::new(5, 42));
-
-    // val_future.poll();
-    // let val = futures::executor::block_on(val_future);
-    // println!("Hello, got: {}", val);
+    executor.send(fut.boxed());
+    // executor.send(build_simple_future().boxed());
+    executor.join();
 }
