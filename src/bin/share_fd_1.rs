e29d5e199cde81de0b242255cb177a6654be8fe8
Mateus Azis <mateusazis@gmail.com>
2023-02-24 14:14:35 -0800

    supposedly working sending fds

--- src/bin/share_fd_1.rs
+++ src/bin/share_fd_1.rs
@@ -1,7 +1,10 @@
+#![feature(unix_socket_ancillary_data)]
+
 use std::error::Error;
 use std::fs::File;
-use std::io::{BufWriter, Write};
-use std::os::unix::net::{SocketAncillary, UnixListener, UnixStream};
+use std::io::Write;
+use std::os::fd::AsRawFd;
+use std::os::unix::net::{SocketAncillary, UnixListener};
 
 fn main_internal() -> Result<(), Box<dyn Error>> {
     let arg: String = std::env::args().last().unwrap();
@@ -9,7 +12,14 @@ fn main_internal() -> Result<(), Box<dyn Error>> {
 
     let listener = UnixListener::bind("/tmp/my_unix_socket")?;
     loop {
-        let (stream, _addr) = listener.accept()?;
+        println!("Waiting for data...");
+        let (mut stream, _addr) = listener.accept()?;
+
+        let mut buf = [0u8; 1];
+        let mut anc = SocketAncillary::new(&mut buf);
+        anc.add_fds(&[f.as_raw_fd()]);
+
+        stream.write(&buf)?;
         // stream.write(buf)
     }
 }
