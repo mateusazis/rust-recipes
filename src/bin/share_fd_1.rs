c1a03400c19ea02f5be922d943db18eea6b233d0
Mateus Azis <mateusazis@gmail.com>
2023-02-27 19:50:42 -0800

    share fd working

--- src/bin/share_fd_1.rs
+++ src/bin/share_fd_1.rs
@@ -2,25 +2,34 @@
 
 use std::error::Error;
 use std::fs::File;
-use std::io::Write;
+use std::io::IoSlice;
 use std::os::fd::AsRawFd;
 use std::os::unix::net::{SocketAncillary, UnixListener};
 
+fn get_file_name() -> String {
+    if std::env::args().len() >= 2 {
+        return std::env::args().last().unwrap();
+    }
+    String::from("/home/azis/windows_dev/rustplay/recipes/src/bin/lorem.txt")
+}
+
 fn main_internal() -> Result<(), Box<dyn Error>> {
-    let arg: String = std::env::args().last().unwrap();
-    let f = File::open(arg.as_str())?;
+    let f = File::open(get_file_name())?;
+    let path = "/tmp/my_unix_socket";
+    std::fs::remove_file(path)?;
 
-    let listener = UnixListener::bind("/tmp/my_unix_socket")?;
+    let listener = UnixListener::bind(path)?;
     loop {
-        println!("Waiting for data...");
-        let (mut stream, _addr) = listener.accept()?;
+        println!("Waiting for client...");
+        let (stream, _addr) = listener.accept()?;
 
-        let mut buf = [0u8; 1];
-        let mut anc = SocketAncillary::new(&mut buf);
+        let buf1 = [0u8; 128];
+        let msgs = [IoSlice::new(&buf1); 1];
+        let mut buf2 = [0u8; 128];
+        let mut anc = SocketAncillary::new(&mut buf2);
         anc.add_fds(&[f.as_raw_fd()]);
 
-        stream.write(&buf)?;
-        // stream.write(buf)
+        stream.send_vectored_with_ancillary(&msgs, &mut anc)?;
     }
 }
 
