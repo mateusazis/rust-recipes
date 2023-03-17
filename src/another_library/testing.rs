b5699b3e14cce29814c1ce84d7f97ee7d7ab1b61
Mateus Azis <mateusazis@gmail.com>
2023-03-17 06:27:55 -0700

    Simplify test references.

--- src/another_library/testing.rs
+++ src/another_library/testing.rs
@@ -1,7 +1,9 @@
 /// Adds two numbers
 ///
 /// ```
-/// assert_eq!(another_library::testing::sum(9,4), 13);
+/// use another_library::testing::sum;
+///
+/// assert_eq!(sum(9,4), 13);
 /// ```
 pub fn sum(a: i32, b: i32) -> i32 {
     a + b
@@ -13,6 +15,6 @@ mod tests {
 
     #[test]
     fn test_sum_returns_the_sum() {
-        assert_eq!(crate::testing::sum(8, 5), 13);
+        assert_eq!(super::sum(8, 5), 13);
     }
 }
