e77ec4f5cf932d071de9d2a622625a45d153c171
Mateus Azis <mateusazis@gmail.com>
2023-06-14 21:09:49 -0700

    SIMD improvements

--- main/src/util/simd.rs
+++ main/src/util/simd.rs
@@ -4,7 +4,7 @@ use std::time::Instant;
 use std::vec::Vec;
 
 // TODO: use a Vec instead; too large to allocate on the stack
-const VEC_LEN: usize = 1024 * 1024 * 8;
+const VEC_LEN: usize = 1024 * 1024 * 32;
 const SIMD_LANES: usize = 8;
 
 fn benchmark(f: &dyn Fn() -> Vec<u32>) -> (Duration, Vec<u32>) {
@@ -23,32 +23,31 @@ pub fn main() {
     }
 
     let (d0, result0) = benchmark(&|| {
-        let mut result0 = Vec::<u32>::with_capacity(VEC_LEN);
-        result0.resize(VEC_LEN, 0);
-        for i in 0..v0.len() {
-            result0[i] = v0[i] + v1[i];
+        let mut result = Vec::<u32>::with_capacity(VEC_LEN);
+        result.resize(VEC_LEN, 0);
+        for i in 0..VEC_LEN {
+            result[i] = v0[i] + v1[i];
         }
-        result0
+        result
     });
 
     let (d1, result1) = benchmark(&|| {
-        let mut result1 = Vec::<u32>::with_capacity(VEC_LEN);
-        result1.resize(VEC_LEN, 0);
-        for i in (0..v0.len()).step_by(SIMD_LANES) {
+        let mut result = Vec::<u32>::with_capacity(VEC_LEN);
+        result.resize(VEC_LEN, 0);
+        for i in (0..VEC_LEN).step_by(SIMD_LANES) {
             let a = Simd::<u32, SIMD_LANES>::from_slice(&v0[i..i + SIMD_LANES]);
             let b = Simd::<u32, SIMD_LANES>::from_slice(&v1[i..i + SIMD_LANES]);
             let sum = a + b;
             let sum_slice = sum.as_array();
-            result1[i..i + SIMD_LANES].copy_from_slice(sum_slice);
+            result[i..i + SIMD_LANES].copy_from_slice(sum_slice);
         }
-        result1
+        result
     });
 
     assert_eq!(result0, result1);
-    // println!("Got: {:?}", result0);
-    // println!("And: {:?}", result1);
     println!(
-        "Without SIMD: {}ms, with SIMD: {}ms",
+        "Added {} elements; without SIMD: {}ms, with SIMD: {}ms",
+        VEC_LEN,
         d0.as_millis(),
         d1.as_millis()
     );
