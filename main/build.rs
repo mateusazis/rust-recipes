ea0ad0e47b2a425de1a084a40defcaf3fcf360b9
Mateus Azis <mateusazis@gmail.com>
2023-06-22 18:19:14 -0700

    fix build script for rpi

--- main/build.rs
+++ main/build.rs
@@ -6,32 +6,26 @@ pub fn main() {
 
     let mut c_flags = String::new();
 
-    if let Ok(target) = std::env::var("TARGET") {
-        c_flags.push_str("-g --target=");
-        c_flags.push_str(target.as_str());
-        if let Ok(os) = std::env::var("CARGO_CFG_TARGET_OS") {
-            if os == "linux" {
-                c_flags.push_str("-fuse-ld=lld");
-            }
-        }
-    }
+    let target = std::env::var("TARGET").unwrap();
+    c_flags.push_str("-g --target=");
+    c_flags.push_str(target.as_str());
 
-    if let Ok(target_os) = std::env::var("CARGO_CFG_TARGET_OS") {
-        if target_os == "macos" {
-            c_flags.push_str(" -lSystem");
-        }
-    }
-
-    if !c_flags.is_empty() {
-        cmd.env("CFLAGS", c_flags.as_str());
-    }
+    let os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    match os.as_str() {
+        "linux" => c_flags.push_str(" -fuse-ld=lld"),
+        "macos" => c_flags.push_str(" -lSystem"),
+        _ => {},
+    };
+    
+    cmd.env("CFLAGS", c_flags.as_str());
+    
 
     let output = cmd.output().expect("should have ran Make");
 
     let code = output.status.code().unwrap();
     if code != 0 {
         let s: std::vec::Vec<String> = output.stderr.lines().map(|line| line.unwrap()).collect();
-        panic!("should have exited with 0, but was {}: {:?}", code, s);
+        panic!("make should have exited with 0, but was {}. Output:\n{:?}", code, s);
     }
 
     for line in output.stdout.lines() {
