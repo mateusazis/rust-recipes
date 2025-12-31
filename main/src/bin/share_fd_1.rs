a884320e68b6fcaa913c9fad974e237d0e095c5b
Mateus Azis <azis@google.com>
2025-12-31 07:08:46 -0300

    make all binaries compilable on mac

--- main/src/bin/share_fd_1.rs
+++ main/src/bin/share_fd_1.rs
@@ -1,48 +1,58 @@
-#![feature(unix_socket_ancillary_data)]
+#![cfg_attr(target_os = "linux", feature(unix_socket_ancillary_data))]
 
 pub mod other_bin_utils;
 
-use other_bin_utils::err_with_backtrace::add_stack_trace;
-use std::error::Error;
-use std::fs::File;
-use std::io::IoSlice;
-use std::os::fd::AsRawFd;
-use std::os::unix::net::{SocketAncillary, UnixListener};
+#[cfg(target_os = "linux")]
+mod internal {
 
-fn get_file_name() -> String {
-    if std::env::args().len() >= 2 {
-        return std::env::args().last().unwrap();
+    use other_bin_utils::err_with_backtrace::add_stack_trace;
+    use std::fs::File;
+    use std::io::IoSlice;
+    use std::os::fd::AsRawFd;
+    use std::os::unix::net::{SocketAncillary, UnixListener};
+
+    fn get_file_name() -> String {
+        if std::env::args().len() >= 2 {
+            return std::env::args().last().unwrap();
+        }
+        String::from("/home/azis/windows_dev/rustplay/recipes2/src/bin/lorem.txt")
     }
-    String::from("/home/azis/windows_dev/rustplay/recipes2/src/bin/lorem.txt")
-}
 
-fn main_internal() -> Result<(), Box<dyn Error>> {
-    let f = add_stack_trace(File::open(get_file_name()))?;
-    let path = "/tmp/my_unix_socket";
-    // don´t care if this fails
-    add_stack_trace(std::fs::remove_file(path)).unwrap_or(());
+    pub fn main_internal() -> Result<(), Box<dyn Error>> {
+        let f = add_stack_trace(File::open(get_file_name()))?;
+        let path = "/tmp/my_unix_socket";
+        // don´t care if this fails
+        add_stack_trace(std::fs::remove_file(path)).unwrap_or(());
 
-    let listener = add_stack_trace(UnixListener::bind(path))?;
-    loop {
-        println!("Waiting for client...");
-        let (stream, _addr) = listener.accept()?;
+        let listener = add_stack_trace(UnixListener::bind(path))?;
+        loop {
+            println!("Waiting for client...");
+            let (stream, _addr) = listener.accept()?;
 
-        let data_buf: [u8; 1] = [0u8; 1];
-        let data_io_slice = IoSlice::new(&data_buf);
+            let data_buf: [u8; 1] = [0u8; 1];
+            let data_io_slice = IoSlice::new(&data_buf);
 
-        let mut ancillary_buf = [0u8; 128];
-        let mut anccillary = SocketAncillary::new(&mut ancillary_buf);
-        anccillary.add_fds(&[f.as_raw_fd()]);
+            let mut ancillary_buf = [0u8; 128];
+            let mut anccillary = SocketAncillary::new(&mut ancillary_buf);
+            anccillary.add_fds(&[f.as_raw_fd()]);
 
-        add_stack_trace(
-            stream.send_vectored_with_ancillary(
+            add_stack_trace(stream.send_vectored_with_ancillary(
                 std::slice::from_ref(&data_io_slice),
                 &mut anccillary,
-            ),
-        )?;
+            ))?;
+        }
     }
+}
+
+#[cfg(not(target_os = "linux"))]
+mod internal {
+  use std::error::Error;
+
+  pub fn main_internal() -> Result<(), Box<dyn Error>> {
+    Ok(())
+  }
 }
 
 fn main() {
-    main_internal().expect("should suceed");
+    internal::main_internal().expect("should suceed");
 }
--- main/src/bin/share_fd_2.rs
+++ main/src/bin/share_fd_2.rs
@@ -1,78 +1,91 @@
-#![feature(unix_socket_ancillary_data)]
+#![cfg_attr(target_os = "linux", feature(unix_socket_ancillary_data))]
 
 pub mod other_bin_utils;
 
-use other_bin_utils::err_with_backtrace::add_stack_trace;
-use std::error::Error;
-use std::fs::File;
-use std::io::{stdin, BufRead, BufReader, IoSliceMut, Read, Seek};
-use std::os::fd::{AsRawFd, FromRawFd, RawFd};
-use std::os::unix::net::{AncillaryData, SocketAncillary, UnixStream};
+#[cfg(target_os = "linux")]
+mod internal {
 
-fn prompt(msg: &str) -> Result<(), std::io::Error> {
-    let mut line = String::new();
-    println!("{}\nPress ENTER to continue...", msg);
-    stdin().lock().read_line(&mut line)?;
-    Ok(())
-}
+    use other_bin_utils::err_with_backtrace::add_stack_trace;
+    use std::error::Error;
+    use std::fs::File;
+    use std::io::{stdin, BufRead, BufReader, IoSliceMut, Read, Seek};
+    use std::os::fd::{AsRawFd, FromRawFd, RawFd};
+    use std::os::unix::net::{AncillaryData, SocketAncillary, UnixStream};
 
-fn get_file_name(fd: RawFd) -> Result<String, std::io::Error> {
-    let link = std::fs::read_link(format!("/proc/self/fd/{}", fd))?;
-    Ok(String::from(link.to_str().unwrap()))
-}
-
-fn get_fds(ancillary: SocketAncillary) -> Vec<RawFd> {
-    let msgs = ancillary.messages();
-    msgs.into_iter()
-        .flat_map(|msg| {
-            println!("Got message from stream");
-            let data = msg.expect("get ancillary data");
-            if let AncillaryData::ScmRights(rights) = data {
-                Vec::from_iter(rights.into_iter())
-            } else {
-                Vec::new()
-            }
-        })
-        .collect()
-}
-
-fn main_internal() -> Result<(), Box<dyn Error>> {
-    println!("Connecting to stream...");
-    let stream = add_stack_trace(UnixStream::connect("/tmp/my_unix_socket"))?;
-    let mut buf1 = [0u8; 128];
-    let mut sli = [IoSliceMut::new(&mut buf1); 1];
-    let mut buf2 = [0u8; 128];
-    let mut ancillary = SocketAncillary::new(&mut buf2);
-    println!("Reading ancillary message...");
-    add_stack_trace(stream.recv_vectored_with_ancillary(&mut sli, &mut ancillary))?;
-
-    let fds = get_fds(ancillary);
-    prompt("Got all messages, about to close the client socket")?;
-    std::mem::drop(stream);
-
-    for fd in fds {
-        let mut f = unsafe { File::from_raw_fd(fd.as_raw_fd()) };
-        f.seek(std::io::SeekFrom::Start(0))?;
-
-        let mut r = BufReader::new(f);
-        let mut content = Vec::new();
-
-        prompt(
-            format!(
-                "About to read file {}...",
-                get_file_name(fd.as_raw_fd()).unwrap()
-            )
-            .as_str(),
-        )?;
-
-        r.read_to_end(&mut content)?;
-        let content = String::from_utf8(content)?;
-        println!("Contents of the file: '{}'", content);
+    fn prompt(msg: &str) -> Result<(), std::io::Error> {
+        let mut line = String::new();
+        println!("{}\nPress ENTER to continue...", msg);
+        stdin().lock().read_line(&mut line)?;
+        Ok(())
     }
-    prompt("about to quit...")?;
-    Ok(())
+
+    fn get_file_name(fd: RawFd) -> Result<String, std::io::Error> {
+        let link = std::fs::read_link(format!("/proc/self/fd/{}", fd))?;
+        Ok(String::from(link.to_str().unwrap()))
+    }
+
+    fn get_fds(ancillary: SocketAncillary) -> Vec<RawFd> {
+        let msgs = ancillary.messages();
+        msgs.into_iter()
+            .flat_map(|msg| {
+                println!("Got message from stream");
+                let data = msg.expect("get ancillary data");
+                if let AncillaryData::ScmRights(rights) = data {
+                    Vec::from_iter(rights.into_iter())
+                } else {
+                    Vec::new()
+                }
+            })
+            .collect()
+    }
+
+    fn main_internal() -> Result<(), Box<dyn Error>> {
+        println!("Connecting to stream...");
+        let stream = add_stack_trace(UnixStream::connect("/tmp/my_unix_socket"))?;
+        let mut buf1 = [0u8; 128];
+        let mut sli = [IoSliceMut::new(&mut buf1); 1];
+        let mut buf2 = [0u8; 128];
+        let mut ancillary = SocketAncillary::new(&mut buf2);
+        println!("Reading ancillary message...");
+        add_stack_trace(stream.recv_vectored_with_ancillary(&mut sli, &mut ancillary))?;
+
+        let fds = get_fds(ancillary);
+        prompt("Got all messages, about to close the client socket")?;
+        std::mem::drop(stream);
+
+        for fd in fds {
+            let mut f = unsafe { File::from_raw_fd(fd.as_raw_fd()) };
+            f.seek(std::io::SeekFrom::Start(0))?;
+
+            let mut r = BufReader::new(f);
+            let mut content = Vec::new();
+
+            prompt(
+                format!(
+                    "About to read file {}...",
+                    get_file_name(fd.as_raw_fd()).unwrap()
+                )
+                .as_str(),
+            )?;
+
+            r.read_to_end(&mut content)?;
+            let content = String::from_utf8(content)?;
+            println!("Contents of the file: '{}'", content);
+        }
+        prompt("about to quit...")?;
+        Ok(())
+    }
+}
+
+#[cfg(not(target_os = "linux"))]
+mod internal {
+    use std::error::Error;
+
+    pub fn main_internal() -> Result<(), Box<dyn Error>> {
+        Ok(())
+    }
 }
 
 fn main() {
-    main_internal().expect("should suceed");
+    internal::main_internal().expect("should suceed");
 }
